5b7f73597e89ae6ac413a734202c2479e6b8f908
HARSHIT SAHU <harshit.sahu@gmail.com>
2023-11-21 14:44:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,18 +11,18 @@ if(searchParams.has("ip")){
     let text =""
     if(hours>=5 && hours<12){
         text+="Good Morning "
-        body.style.backgroundImage=`url("./morning.jpg")`
+        body.style.backgroundImage=`url("../morning.jpg")`
         quote.innerText="Morning is nature's way of reminding us that every day is a new canvas waiting to be painted with the brushstrokes of possibility and opportunity."
     }
     else if(hours>=12 && hours<18){
         text+="Good Afternoon "
-        body.style.backgroundImage=`url("./afternoon.jpg")`
+        body.style.backgroundImage=`url("../afternoon.jpg")`
         body.style.color="yellow"
         quote.innerText="Afternoon is a gentle embrace between the hustle of the morning and the calm of the evening—a moment to catch your breath and savor the warmth of the day's journey."
     }
     else{
         text+="Good Night "
-        body.style.backgroundImage=`url("./night.jpeg")`
+        body.style.backgroundImage=`url("../night.jpeg")`
         body.style.color="whitesmoke"
         quote.innerText="In the stillness of the night, the world whispers its secrets, and dreams unfold like constellations in the vast canvas of the sky, painting the darkness with the magic of possibilities."
     }
